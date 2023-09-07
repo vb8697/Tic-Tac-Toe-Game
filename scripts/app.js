7c14bb8114f61d6d7fb740545e46a74c44c73fa5
VIKASH <vikash@VIKASHs-MacBook-Pro.local>
2023-09-07 12:12:45 +0530

    adding the tic tac toe game

---
+++ scripts/app.js
@@ -0,0 +1,26 @@
+let editedPlayer = 0;
+const players=[
+    {
+        name:'',
+        symbol:'X',
+    },{
+
+    },
+];
+const confOverlayElement=document.getElementById('config-overlay');
+const backdropElement=document.getElementById('backdrop');
+const formElement=document.querySelector('form');
+const errorsOutputElement=document.getElementById('config-errors');
+
+const editPlayer1BtnElement=document.getElementById('edit-player-1-btn');
+const editPlayer2BtnElement=document.getElementById('edit-player-2-btn');
+const cancelConfBtnElement=document.getElementById('cancel-conf-btn');
+const startNewGameBtnElement=document.getElementById('start-game-btn');
+
+editPlayer1BtnElement.addEventListener('click',openPlayerConfig);
+editPlayer2BtnElement.addEventListener('click',openPlayerConfig);
+
+cancelConfBtnElement.addEventListener('click',closePlayerConfig);
+backdropElement.addEventListener('click',closePlayerConfig);
+
+formElement.addEventListener('submit',savePlayerConfig);
---
+++ scripts/config.js
@@ -0,0 +1,24 @@
+
+function openPlayerConfig(event){
+   editedPlayer=+event.target.dataset.playerid; 
+    confOverlayElement.style.display= 'block';
+    backdropElement.style.display= 'block';
+}  
+function closePlayerConfig(){
+    confOverlayElement.style.display= 'none';
+    backdropElement.style.display= 'none';
+    formElement.firstElementChild.classList.remove('error');
+    errorsOutputElement.textContent= ' ';
+}
+function savePlayerConfig(event){
+    event.preventDefault();
+    const formData=new FormData(event.target);
+    const enteredPlayername=formData.get('playername').trim();
+   if(!enteredPlayername){
+        event.target.firstElementChild.classList.add('error'); 
+        errorsOutputElement.textContent='Please enter a valid name!';
+        return;
+    }
+    const updatedPlayerDataElement=document.getElementById('player-' + editedPlayer + '-data');
+    updatedPlayerDataElement.children[1].textContent=enteredPlayername;
+} 
